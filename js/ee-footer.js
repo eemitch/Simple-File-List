c279f4e7867b7f714b2bb4de93240e2e86c833be
Mitchell Bennis <48490506+eemitch@users.noreply.github.com>
2022-03-07 11:17:31 -0600

    5.0.3
    Removed media player testing script

--- js/ee-footer.js
+++ js/ee-footer.js
@@ -14,46 +14,7 @@ jQuery(document).ready(function($) {
 	
 	jQuery('#eeSFL_Modal_Manage_Close').on('click', function() {
 		jQuery('#eeSFL_Modal_Manage').hide();
-	});	
-	
-	
-	
-	
-	
-	
-	
-	
-	// Look for Media Files and Add Player
-	jQuery( '.eeFiles tr' ).each(function( index ) {
-		
-		// Get the name of this row's ID
-		var eeThisID = '#' + jQuery(this).attr('id'); // TO DO - Change id to class to class in SFL 5/6
-		var eeFileName = jQuery( eeThisID + " span.eeSFL_RealFileName" ).text(); // Get the File Name
-		var eeFileLink = jQuery( eeThisID + " a.eeSFL_FileName" ).attr('href'); // Get the File Link
-		var eeExt = eeFileName.split('.').pop(); // Get the File Extension
-		console.log( index + ": " + eeFileName );
-		
-		var eeAudioPlayer = '<audio class="eeSFL_AudioPlayer" controls><source src="' + eeFileLink + '" type="audio/mpeg">Not Supported</audio>';
-		
-		if(eeExt == 'mp3') {
-			console.log('MP3 File Found: ' + eeFileName);
-			console.log('Updating the Display...');
-			
-			// Change the Open Link to Play
-			jQuery(eeThisID + ' .eeSFL_FileOpen').text('Play');
-			
-			jQuery( eeThisID + " td.eeSFL_FileName" ).append('<div class="eeSFL_AudioPlayerWrap">' + eeAudioPlayer +  '</div>'); // TO DO - Update this for SFL 5/6
-			
-			
-		}
-		
-		
-		
-		
-		
-		
 	});
-	
 		
 
 }); // END Ready Function
